9961b4035bf912857ea4c9435a0f593e964cad7d
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-06-19 12:25:30 +0200

    Create basic version of AI vs AI game

--- BattleShipGame/Game/GameMenu/GameMenuAtributors.cs
+++ BattleShipGame/Game/GameMenu/GameMenuAtributors.cs
@@ -11,9 +11,10 @@ namespace ConsoleApp7.Game.GameMenu
         {
             WriteLine("\n[1] Start new console human vs human game\n" +
                           "[2] Start new console human vs AI game\n" +
-                          "[3] Start new LAN human vs human game\n" +
-                          "[4] Show high scores\n"+
-                          "[5] Exit game\n");
+                          "[3] Watch AI vs AI game\n" +
+                          "[4] Start new LAN human vs human game\n" +
+                          "[5] Show high scores\n"+
+                          "[6] Exit game\n");
         }
 
         public static int DisplayMenuChoice()
@@ -41,7 +42,13 @@ namespace ConsoleApp7.Game.GameMenu
                         gameWithComputer.DisplayEndGameMessage();
                         break;
                     case 3:
-                        WriteLine("Not implemented yet");
+                        var gameAIvsAi = new GameEngine.Game(
+                            new PlayerAI(new ConsoleDisplay()),
+                            new PlayerAI(new ConsoleDisplay())
+                        );
+                        gameAIvsAi.GameLoop();
+                        gameAIvsAi.DisplayEndGameMessage();
+                        break;
                         break;
                     case 4:
                         WriteLine("Not implemented yet");
