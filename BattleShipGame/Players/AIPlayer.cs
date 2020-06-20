bc539c32e8f3a222da47c16e65f28cc9abd8b813
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-06-20 09:59:56 +0200

    clear AI player

--- BattleShipGame/Players/AIPlayer.cs
+++ BattleShipGame/Players/AIPlayer.cs
@@ -13,7 +13,6 @@ namespace ConsoleApp7.Players
 
     public class PlayerAI : Player
     {
-        private int _round; //ONLY FOR TESTS
         private List<int[]> UniqueShootsArray = new List<int[]>();
         private List<int []> PositionsOfHitShip { get; set; } = new List<int[]>();
 
@@ -35,29 +34,8 @@ namespace ConsoleApp7.Players
             PlayerBoard = new Ocean(10, 10);
             Display = display;
             DifficultyLevel = SetDifficultyLevel();
-            // if (DifficultyLevel == Difficulty.Hard) 
-            WeightsOfShootsForHard = InitWeightList();
             PlayerNick = $"Computer {DifficultyLevel.ToString()}";
             SetShips(PlayerBoard, Display.DisplayOcean);
-        }
-
-        private int[,] InitWeightList()
-        { 
-            int maxSizeX = 9; // as array index
-            int maxSizeY = 9; // as array index
-            var tempArrToReturn = new int [maxSizeX + 1, maxSizeY + 1];
-            
-            bool IsBoardCorner(int i, int j) => (i == 0 || i == maxSizeX) && (j == 0 || j == maxSizeY);
-            bool IsBoardBorder(int i, int j) => j == 0 || i == 0 || j == maxSizeY || i == maxSizeX;
-            
-            for (int i = 0; i <= maxSizeX; i++)
-            {
-                for (int j = 0; j <= maxSizeY; j++)
-                {
-                    tempArrToReturn[i,j] = IsBoardCorner(i, j) ? 2 : IsBoardBorder(i, j) ? 3 : 4;
-                }
-            }
-            return tempArrToReturn;
         }
         public override string Attack()
         {
@@ -71,73 +49,21 @@ namespace ConsoleApp7.Players
                     return HardAttack();
             }
         }
-        
         private string EasyAttack()
         {
             // Easy attack is totally random attack even though CPU hit into ship
             // it will still generate random positions
+            
             var randomPositionsAttack = Utils.GenerateAndMakeUniqueRandomArray(UniqueShootsArray);
             return Utils.ConvertXYtoStringRepresentationOfCords(randomPositionsAttack);
         }
-
-        private string KillShipIfShoot()
-        {
-            foreach (var shipPosition in PositionsOfHitShip)
-            {
-                var shipPos = new OceanFieldValidator(shipPosition, PlayerBoard, UniqueShootsArray);
-
-                if (!ShipOrientationIsKnown) return shipPos.GetAsString(shipPos.GetValidFieldAround());
-
-                if (IsShipHorizontal) if (shipPos.IsValidHorizontal) return shipPos.GetAsString(shipPos.GetValidHorizontal());
-                if (!IsShipHorizontal) if (shipPos.IsValidVertical) return shipPos.GetAsString(shipPos.GetValidVertical());
-            }
-            return null;
-        }
-
         private string MediumAttack()
         {
             //Medium attack randomly search for ship but when it hit into ship the ship will be
             //destroyed in less possible moves
-            /*_round++;
-            if (_round == 1) return "A1"; // ONLY FOR TESTS*/
+            
             if (!IsShipHitNotSink) return EasyAttack();
             return KillShipIfShoot();
-        }
-
-        private int [] ChoseMaxWeightFromList()
-        {
-            int posX = 0;
-            int posY = 0;
-            int maxValue = 0;
-            // iterate over list of arrays
-            for (int i = 0; i < 10; i++)
-            {
-                // iterate over array of elements in order to find max element
-                for (int j = 0; j < 10; j++)
-                {
-                    if (maxValue < WeightsOfShootsForHard[i,j])
-                    {
-                        maxValue = WeightsOfShootsForHard[i,j];
-                        posX = i;
-                        posY = j;
-                    }
-                }
-            }
-            return new [] {posX, posY};
-        }
-        
-        
-        private void UpdateWeightsBoardAfterAttack(int [] attackedPos) 
-        {
-            WeightsOfShootsForHard[attackedPos[0] ,attackedPos[1]] = 0;
-            var posAttack = new OceanFieldValidator(attackedPos, PlayerBoard, new List<int[]>());
-            
-            while(posAttack.IsValidFieldAround)
-            {
-                var posXy = posAttack.GetValidFieldAround(); // x, y
-                posAttack.AddToForbiddenList(posXy);
-                WeightsOfShootsForHard[posXy[0], posXy[1]]--;
-            }
         }
         private string HardAttack()
         {
@@ -155,46 +81,34 @@ namespace ConsoleApp7.Players
             {
                 var attackedPosition = ChoseMaxWeightFromList();
                 UpdateWeightsBoardAfterAttack(attackedPosition);
-                
                 return Utils.ConvertXYtoStringRepresentationOfCords(attackedPosition);
             }
+            //:TODO Refactor 
             var positionToAttack = KillShipIfShoot();
             var attackedPos = Utils.ConvertAttackedPositionToXy(positionToAttack);
             var fakedAttackedPositionAsXy = new [] {attackedPos[1], attackedPos[0]};
             UpdateWeightsBoardAfterAttack(fakedAttackedPositionAsXy);
             return positionToAttack;
-            
-            
         }
-        
-        private static int GenerateOrientation() => Utils.GenerateRandomFromToRange(1, 2);
-
-        private int [] ShipFirstFieldPosition(Ship shipToCheck)
+        private string KillShipIfShoot()
         {
-            var posXy = new int[2];
-            do
+            foreach (var shipPosition in PositionsOfHitShip)
             {
-                posXy[0] = Utils.GenerateRandomFromToRange();
-                posXy[1] = Utils.GenerateRandomFromToRange();
-            } while (!Validation.IsSpaceForShip(posXy, PlayerBoard, shipToCheck));
-            
-            return posXy;
+                var shipPos = new OceanFieldValidator(shipPosition, PlayerBoard, UniqueShootsArray);
+
+                if (!ShipOrientationIsKnown) return shipPos.GetAsString(shipPos.GetValidFieldAround());
+
+                if (IsShipHorizontal) if (shipPos.IsValidHorizontal) return shipPos.GetAsString(shipPos.GetValidHorizontal());
+                if (!IsShipHorizontal) if (shipPos.IsValidVertical) return shipPos.GetAsString(shipPos.GetValidVertical());
+            }
+            return null;
         }
+
         private bool CheckIfOrientationIsHorizontal(int [] attackedPosition)
         {   
             return PositionsOfHitShip[0][0] + 1 == attackedPosition[0] || PositionsOfHitShip[0][0] - 1 == attackedPosition[0];
         }
-        protected override void SetShips(Ocean playerBoard, IOceanDisplay oceanDisplay)
-        {
-            var fleetForAi = ShipsCreation.CreateFleet();
 
-            foreach (var shipAi in fleetForAi)
-            {
-                shipAi.Orientation = GenerateOrientation();
-                shipAi.StartPositions = ShipFirstFieldPosition(shipAi); 
-                playerBoard.AddNewShip(shipAi);
-            }
-        }
         public override bool[] UpdateMyBoard(string attackPosition)
         {
             var attackedPositionXy = Utils.ConvertAttackedPositionToXy(attackPosition);
@@ -220,20 +134,80 @@ namespace ConsoleApp7.Players
                 if (PositionsOfHitShip.Count == 2) IsShipHorizontal = CheckIfOrientationIsHorizontal(fakedAttackedPositionAsXy);
             }
         }
-
-        private void MarkAsShotAllPositionsAroundSunkShip()
-        {   //: TODO move to other place
-            foreach (var shipPosition in PositionsOfHitShip)
+        private int [] ChoseMaxWeightFromList()
+        {
+            int posX = 0;
+            int posY = 0;
+            int maxValue = 0;
+            // iterate over list of arrays
+            for (int i = 0; i < 10; i++)
             {
-                var shipPos = new OceanFieldValidator(shipPosition, PlayerBoard, UniqueShootsArray);
-                if (shipPos.IsValidFieldAround)
+                // iterate over array of elements in order to find max element
+                for (int j = 0; j < 10; j++)
                 {
-                    var fieldToShot = shipPos.GetValidFieldAround();
-                    PlayerBoard.GetShot(fieldToShot);
-                    UniqueShootsArray.Add(fieldToShot);
-                } 
-            } 
+                    if (maxValue < WeightsOfShootsForHard[i,j])
+                    {
+                        maxValue = WeightsOfShootsForHard[i,j];
+                        posX = i;
+                        posY = j;
+                    }
+                }
+            }
+            return new [] {posX, posY};
         }
+        private int[,] InitWeightList()
+        { 
+            int maxSizeX = 9; // as array index
+            int maxSizeY = 9; // as array index
+            var tempArrToReturn = new int [maxSizeX + 1, maxSizeY + 1];
+            
+            bool IsBoardCorner(int i, int j) => (i == 0 || i == maxSizeX) && (j == 0 || j == maxSizeY);
+            bool IsBoardBorder(int i, int j) => j == 0 || i == 0 || j == maxSizeY || i == maxSizeX;
+            
+            for (int i = 0; i <= maxSizeX; i++)
+            {
+                for (int j = 0; j <= maxSizeY; j++)
+                {
+                    tempArrToReturn[i,j] = IsBoardCorner(i, j) ? 2 : IsBoardBorder(i, j) ? 3 : 4;
+                }
+            }
+            return tempArrToReturn;
+        }
+        private void UpdateWeightsBoardAfterAttack(int [] attackedPos) 
+        {
+            WeightsOfShootsForHard[attackedPos[0] ,attackedPos[1]] = 0;
+            var posAttack = new OceanFieldValidator(attackedPos, PlayerBoard, new List<int[]>());
+            
+            while(posAttack.IsValidFieldAround)
+            {
+                var posXy = posAttack.GetValidFieldAround(); // x, y
+                posAttack.AddToForbiddenList(posXy);
+                WeightsOfShootsForHard[posXy[0], posXy[1]]--;
+            }
+        }
+        protected override void SetShips(Ocean playerBoard, IOceanDisplay oceanDisplay)
+        {
+            var fleetForAi = ShipsCreation.CreateFleet();
+
+            foreach (var shipAi in fleetForAi)
+            {
+                shipAi.Orientation = GenerateOrientation();
+                shipAi.StartPositions = ShipFirstFieldPosition(shipAi); 
+                playerBoard.AddNewShip(shipAi);
+            }
+        }
+        private int [] ShipFirstFieldPosition(Ship shipToCheck)
+        {
+            var posXy = new int[2];
+            do
+            {
+                posXy[0] = Utils.GenerateRandomFromToRange();
+                posXy[1] = Utils.GenerateRandomFromToRange();
+            } while (!Validation.IsSpaceForShip(posXy, PlayerBoard, shipToCheck));
+            
+            return posXy;
+        }
+        private static int GenerateOrientation() => Utils.GenerateRandomFromToRange(1, 2);
         private static void PrintDifficultyOptionsToSelect()
         {
             Console.Clear();
@@ -262,6 +236,7 @@ namespace ConsoleApp7.Players
                 case (int)Difficulty.Medium:
                     return Difficulty.Medium;
                 case (int)Difficulty.Hard:
+                    WeightsOfShootsForHard = InitWeightList();
                     return Difficulty.Hard;
             }
             return Difficulty.Easy;
